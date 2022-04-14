97a4952a7273ecb99160f385a30dcd3fe993b360
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-04-14 14:38:02 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -1,3 +1,38 @@
+// start api
+
+import React, { useEffect, useState } from 'react';
+import { Link } from '@reach/router';
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const resp = await fetch('https://serverless-api.signalnerve.workers.dev/api/posts');
+      const postsResp = await resp.json();
+      setPosts(postsResp);
+    };
+
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
+
+//end api clodflare
 // start react json script
 
 import React, { useEffect, useState } from 'react';
