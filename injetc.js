089842a93a4840fd4573a8c83fb7a0977b2ae323
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-04-15 01:10:57 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -579,7 +579,10 @@ import * as data from "https://faucetpay.io/dash/lib/datatables.net-responsive/j
 
 <!---import * as data from "https://www.google.com/xjs/_/js/k=xjs.qs.id.zLPPe5QdokI.O/ck=xjs.qs.W9_vfKMOppA.L.W.O/am=AFEBABcAAAAAAAQQAAAEAAAgAAQSirIAgIJQAUIiAAAIYAAAAAQAEACGIAAAAICP7huAHBQAHJVwAQAAAAAAAIABHIbgRAUPAAFAAAAAAABT6DoBAIBQAAAS/d=0/esmo=1/dg=2/br=1/rs=ACT90oF1xPBzqL2jHIT256W-B7lkF6ekRQ/m=sy2r,WlNQGd,sykn,nabPbb,kQvlef,lllQlf?xjs=s2";----->
 
-(
+// import module
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+//
 
 //<!----// Fire the loading--->
 
