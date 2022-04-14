bdd087219e9027d9613e31db16a6a697383bf534
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-04-14 14:01:47 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -1,4 +1,45 @@
+// start react json script
 
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://raw.githubusercontent.com/GMhabib/index.js/main/injetc.js');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+
+//end of react video
 
 function loadScript(url, callback) {
   
