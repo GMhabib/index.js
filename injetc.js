67c17461dde6f59af32cf852db344c523d6318d1
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-05-01 00:24:14 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -1,12 +1,9 @@
 // install start
-<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-<script
-  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-  crossorigin></script>
-<script
+  src="https://unpkg.com/react/umd/react.production.min.js" crossorigin;
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin;
   src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-  crossorigin></script>
-<script>var Alert = ReactBootstrap.Alert;</script>
+  crossorigin;
+var Alert = ReactBootstrap.Alert;
 // install end
 
 // start effect
