ec086a8e1a4c2935e9f98ecc527b5d64f40b55f3
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-05-01 00:20:00 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -1,3 +1,14 @@
+// install start
+<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+<script
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script>
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
+<script>var Alert = ReactBootstrap.Alert;</script>
+// install end
+
 // start effect
 useEffect(() => {
   const subscription = props.source.subscribe();
