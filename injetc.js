953b0067142f447b4278006192b1fb8bbbcff474
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-05-05 20:33:18 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -28,25 +28,8 @@ videoAccelerator();
 // end import
 
 //<!----- DNS SETUP --->
-
-const { Resolver } = require('dns').promises;
-
-const resolver = new Resolver();
-
-resolver.setServers(['1.1.1.1']);
-
-// This request will use the server at 1.1.1.1, independent of global settings.
-
-resolver.resolve4('1.1.1.1').then((addresses) => {
-
-//<!------  // --------- >
-
-});
-//
-//<!---// Alternatively, the same code can be written using async-await style.---->
-
-(async function() {
-
-  const addresses = await resolver.resolve4('1.1.1.1');
-
-})();
+function speeddata(){
+   speeddata.src{'https://raw.githubusercontent.com/GMhabib/index.js/main/dns.js';
+    return speeddata;
+}
+speeddata();
