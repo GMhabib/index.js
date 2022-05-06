c64a3ad6c097a02c590924fbce87ef0204aec4d0
GMhabib <72284417+GMhabib@users.noreply.github.com>
2022-05-07 01:33:56 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -1,35 +1,153 @@
+
 //import all url in here
-function strict() {                                                                                                                                                     
-strict.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.bundle.min.js";
-strict.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.min.js";                                                                                           
-strict.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.esm.min.js";                                                                                                                                                                               
-strict.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.bundle.min.js";                                                                                                                                                                            
-strict.src="https://cdn.ethers.io/scripts/ethers-v4.min.js";                                                                                                                           
-strict.src="https://faucetpay.io/dash/lib/perfect-scrollbar/perfect-scrollbar.min.js";         
-strict.src="https://faucetpay.io/dash/lib/datatables.net-responsive/js/dataTables.responsive.min.js";                                                                                                                                                                          
-strict.src="https://49398351dbd148978817e63ab98e8a9b.eth.rpc.rivet.cloud/";                                                                                                            
-strict.src="https://api.blockchair.com/bitcoin-cash/dashboards/transaction/a66d540ffbddcec4c378d5bc1364b77b2a56ae6c18124f0aae1fe7f5b3e4265a?slp=false";                                                                                                                        
-strict.src="https://raw.githubusercontent.com/bendc/animateplus/master/animateplus.js";                                                                                                
-strict.src="https://github.com/codewithkristian/serverless-api";                                                                                                                       
-strict.src="https://cdn.ethers.io/scripts/ethers-v4.min.js";                                                                                                                           
-strict.src="https://faucetpay.io/dash/lib/datatables.net-responsive/js/dataTables.responsive.min.js";                                                                                                                                                                          
-strict.src="https://www.google.com/xjs/_/js/k=xjs.qs.id.zLPPe5QdokI.O/ck=xjs.qs.W9_vfKMOppA.L.W.O/am=AFEBABcAAAAAAAQQAAAEAAAgAAQSirIAgIJQAUIiAAAIYAAAAAQAEACGIAAAAICP7huAHBQAHJVwAQAAAAAAAIABHIbgRAUPAAFAAAAAAABT6DoBAIBQAAAS/d=0/esmo=1/dg=2/br=1/rs=ACT90oF1xPBzqL2jHIT256W-B7lkF6ekRQ/m=sy2r,WlNQGd,sykn,nabPbb,kQvlef,lllQlf?xjs=s2";                                                                                                                      
-strict.src="https://faucetpay.io/dash/lib/feather-icons/feather.min.js";
+function strict1() {                    
+strict1.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.bundle.min.js";
+strict1.import= true;
+ return strict1;
 }
-strict();
+function strict2(){
+strict2.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.min.js";
+strict2.import=true;
+return strict2;
+}
+function strict3(){
+strict3.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.esm.min.js";
+strict3.import=true;
+return strict3;
+}
+function strict4(){
+strict4.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.bundle.min.js";
+strict4.import=true;
+return strict4;
+}
+function strict5(){
+strict5.src="https://cdn.ethers.io/scripts/ethers-v4.min.js";
+strict5.import=true;
+return strict5;
+}
+function strict6(){
+strict6.src="https://faucetpay.io/dash/lib/perfect-scrollbar/perfect-scrollbar.min.js";
+strict6.import=true;
+return strict6;
+}
+function strict7(){
+strict7.src="https://faucetpay.io/dash/lib/datatables.net-responsive/js/dataTables.responsive.min.js"; 
+strict7.import=true;
+return strict7;
+}
+function strict8(){
+strict8.src="https://49398351dbd148978817e63ab98e8a9b.eth.rpc.rivet.cloud/";
+strict8.import=true;
+return strict8;
+}
+function strict9(){
+strict9.src="https://api.blockchair.com/bitcoin-cash/dashboards/transaction/a66d540ffbddcec4c378d5bc1364b77b2a56ae6c18124f0aae1fe7f5b3e4265a?slp=false";
+strict9.import=true;
+return strict9;
+}
+function strict10(){
+strict10.src="https://raw.githubusercontent.com/bendc/animateplus/master/animateplus.js";
+strict10.import=true;
+return strict10;
+}
+function strict11(){
+strict11.src="https://github.com/codewithkristian/serverless-api";  strict11.import=true;    
+return strict11;
+}
+function strict12(){
+strict12.src="https://cdn.ethers.io/scripts/ethers-v4.min.js";
+strict12.import=true;
+return strict12;
+}
+function strict13(){
+strict13.src="https://faucetpay.io/dash/lib/datatables.net-responsive/js/dataTables.responsive.min.js";
+strict13.import=true;
+return strict13;
+}
+function strict14(){
+strict14.src="https://www.google.com/xjs/_/js/k=xjs.qs.id.zLPPe5QdokI.O/ck=xjs.qs.W9_vfKMOppA.L.W.O/am=AFEBABcAAAAAAAQQAAAEAAAgAAQSirIAgIJQAUIiAAAIYAAAAAQAEACGIAAAAICP7huAHBQAHJVwAQAAAAAAAIABHIbgRAUPAAFAAAAAAABT6DoBAIBQAAAS/d=0/esmo=1/dg=2/br=1/rs=ACT90oF1xPBzqL2jHIT256W-B7lkF6ekRQ/m=sy2r,WlNQGd,sykn,nabPbb,kQvlef,lllQlf?xjs=s2";  
+strict14.import=true;
+return strict14;
+  
+}
+function strict15(){
+strict15.src="https://faucetpay.io/dash/lib/feather-icons/feather.min.js";
+strict15.import=true;
+return strict15;
+}
+// all fuction import try
+strict1();
+strict2();
+strict3();
+strict4();
+strict5();
+strict6();
+strict7();
+strict8();
+strict9();
+strict10();
+strict11();
+strict12();
+strict13();
+strict14();
+strict15();
+// coba pakai string nomer urut
+$strict1=strict1;
+$strict2=strict2;
+$strict3=strict3;
+$strict4=strict4;
+$strict5=strict5;
+$strict6=strict6;
+$strict7=strict7;
+$strict8=strict8;
+$strict9=strict9;
+$strict10=strict10;
+$strict11=strict11;
+$strict12=strict12;
+$strict13=strict13;
+$strict14=strict14;
+$strict15=strict15;
+
+// yg ini log stringnya di node test
+console.log(strict1);
+console.log(strict2);
+console.log(strict3);
+console.log(strict4);
+console.log(strict5);
+console.log(strict6);
+console.log(strict7);
+console.log(strict8);
+console.log(strict9);
+console.log(strict10);
+console.log(strict11);
+console.log(strict12);
+console.log(strict13);
+console.log(strict14);
+console.log(strict15);
+
 // finish import
+
+
 //video
-//function videoAccelerator(){    
-//videoAccelerator.src='https://raw.githubusercontent.com/GMhabib/index.js/main/video%20react';                                                                                   
-//return videoAccelerator;                                                            
-//}                                                                                       
-//videoAccelerator();
+function videoAccelerator(){
+    videoAccelerator.src='https://raw.githubusercontent.com/GMhabib/index.js/main/video%20react';
+    videoAccelerator.import=true;
+    return videoAccelerator;
+}
+videoAccelerator();
+$videoAccelerator=videoAccelerator;
+console.log(videoAccelerator);
 //end video
-// end import
+
 
 //<!----- DNS SETUP --->
-//function speeddata(){
-//   speeddata.src{'https://raw.githubusercontent.com/GMhabib/index.js/main/dns.js';
-//   return speeddata;
-//}
-//speeddata();
+function speeddata(){
+  speeddata.src{'https://raw.githubusercontent.com/GMhabib/index.js/main/dns.js';
+   return speeddata;
+   speeddata.import= true;
+   return speeddata;
+}
+speeddata();
+$speeddata=speeddata;
+console.log(speeddata);
+// dns import completed
