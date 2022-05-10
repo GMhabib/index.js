ca99f2e0ca18955a75cb06e40222b74c1a50b687
habibGM <72284417+GMhabib@users.noreply.github.com>
2022-05-10 23:40:54 +0700

    Update injetc.js

--- injetc.js
+++ injetc.js
@@ -6,6 +6,7 @@ this.header='Content-type', 'text/javascript';
     this.setHeader="Cache-Control", "no-cache, no-store, must-revalidate";
 this.setHeader="Pragma", "no-cache";
 this.setDateHeader="Expires", 0;
+return response;
 }
 response();
 $response=response;
@@ -16,83 +17,81 @@ console.log(response);
 function strict1() {                    
 strict1.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.bundle.min.js";
 strict1.import='true';
- return strict1;
+ 
 }
 function strict2(){
 strict2.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.min.js";
 strict2.import='true';
-return strict2;
+
 }
 function strict3(){
 strict3.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.esm.min.js";
 strict3.import='true';
-return strict3;
+
 }
 function strict4(){
 strict4.src="https://raw.githubusercontent.com/twbs/bootstrap/main/dist/js/bootstrap.bundle.min.js";
 strict4.import='true';
-return strict4;
+
 }
 function strict5(){
 strict5.src="https://cdn.ethers.io/scripts/ethers-v4.min.js";
 strict5.import='true';
-return strict5;
 }
 function strict6(){
 strict6.src="https://faucetpay.io/dash/lib/perfect-scrollbar/perfect-scrollbar.min.js";
 strict6.import='true';
-return strict6;
+
 }
 function strict7(){
 strict7.src="https://faucetpay.io/dash/lib/datatables.net-responsive/js/dataTables.responsive.min.js"; 
 strict7.import='true';
-return strict7;
+
 }
 function strict8(){
 strict8.src="https://49398351dbd148978817e63ab98e8a9b.eth.rpc.rivet.cloud/";
 strict8.import='true';
-return strict8;
+
 }
 function strict9(){
 strict9.src="https://api.blockchair.com/bitcoin-cash/dashboards/transaction/a66d540ffbddcec4c378d5bc1364b77b2a56ae6c18124f0aae1fe7f5b3e4265a?slp=false";
 strict9.import='true';
-return strict9;
+
 }
 function strict10(){
 strict10.src="https://raw.githubusercontent.com/bendc/animateplus/master/animateplus.js";
 strict10.import='true';
-return strict10;
+
 }
 function strict11(){
 strict11.src="https://github.com/codewithkristian/serverless-api";  
 strict11.import='true';    
-return strict11;
+
 }
 function strict12(){
 strict12.src="https://cdn.ethers.io/scripts/ethers-v4.min.js";
 strict12.import='true';
-return strict12;
+
 }
 function strict13(){
 strict13.src="https://faucetpay.io/dash/lib/datatables.net-responsive/js/dataTables.responsive.min.js";
 strict13.import='true';
-return strict13;
+
 }
 function strict14(src){
 strict14.src="https://www.google.com/xjs/_/js/k=xjs.qs.id.zLPPe5QdokI.O/ck=xjs.qs.W9_vfKMOppA.L.W.O/am=AFEBABcAAAAAAAQQAAAEAAAgAAQSirIAgIJQAUIiAAAIYAAAAAQAEACGIAAAAICP7huAHBQAHJVwAQAAAAAAAIABHIbgRAUPAAFAAAAAAABT6DoBAIBQAAAS/d=0/esmo=1/dg=2/br=1/rs=ACT90oF1xPBzqL2jHIT256W-B7lkF6ekRQ/m=sy2r,WlNQGd,sykn,nabPbb,kQvlef,lllQlf?xjs=s2";  
 strict14.import='true';
-return strict14;
   
 }
 function strict15(){
 strict15.src="https://faucetpay.io/dash/lib/feather-icons/feather.min.js";
 strict15.import='true';
-return strict15;
+
 }
 function strict16() {
 strict16.src="https://jsc.mgid.com/m/e/mercusuar.uzone.id.1224335.es6.js";
 strict16.import='true';
-return strict16;
+
 }
 // all fuction import try
 strict1();
